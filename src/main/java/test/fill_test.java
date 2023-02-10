79d8222f6edd414c7e46b5707ca84340fa2f290c
승현 <@LAPTOP-G5TNKQGT>
2023-02-10 16:35:52 +0900

    fill 예문 테스트

---
+++ src/main/java/test/fill_test.java
@@ -0,0 +1,25 @@
+package test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class fill_test {
+
+	public static void main(String[] args) {
+		int[] arrayTest1 = new int[10];
+		int[] arrayTest2 = new int[10];
+		
+		for(int i=0;i<arrayTest1.length; i++) {
+			arrayTest1[i]=10;
+		}
+		
+		Arrays.fill(arrayTest2, 10);
+		
+		StringBuilder sb1 = new StringBuilder();
+		StringBuilder sb2 = new StringBuilder();
+		
+		
+		
+	}
+
+}
