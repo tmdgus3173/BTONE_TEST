35d7267ca0a2915dc9edf732b297d51c4f5b96a5
승현 <@LAPTOP-G5TNKQGT>
2023-02-15 17:56:43 +0900

    [오후] 중요한 for문 공부

---
+++ src/main/java/Vo0211/Ex02.java
@@ -0,0 +1,43 @@
+package Vo0211;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		List<String> nameList = Arrays.asList("이유덕", "이재영", "권종표", "이재영", "박민호", "강상희", "이재영", "김지완", "최승혁", "이성연", "박영서", "박민호", "전경헌",
+				"송정환", "김재성", "이유덕", "전경헌");
+		
+		StudentVo studentVo = new StudentVo();
+	
+		System.out.println(nameList);
+		
+		
+		
+//		nameVoList.add(nameVo);
+//		System.out.println(nameVoList);
+//		[{name : "이유덕"}]
+		
+		// nameList의 이름중 이씨만 studentVo의 이름에 넣기 
+		List<NameVo> nameVoList = new ArrayList<>();
+		
+		for(int i= 0; i < nameList.size(); i++) {
+			
+			NameVo nameVo = new NameVo();
+			if(nameList.get(i).substring(0,1).equals("이")) {
+				
+				nameVo.setName(nameList.get(i));
+				
+				nameVoList.add(nameVo);
+				
+			}
+			
+			
+		}
+		studentVo.setList(nameVoList);
+		System.out.println(studentVo.getList());
+		
+	}
+	
+}
